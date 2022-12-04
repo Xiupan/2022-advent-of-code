bb57ba57da9009179324e8784439a35c7fb2fb47
Alan Hong <xiupan@gmail.com>
2022-12-04 12:25:45 -0600

    feat: day 4 problem 1 complete!

---
+++ day-4/problem1.js
@@ -0,0 +1,39 @@
+import { input } from "./input.js";
+import _ from "lodash";
+
+const inputArray = input.split("\n");
+
+let completeOverlaps = 0;
+
+inputArray.forEach((line) => {
+  // each line is a set of two elves' tasks
+  const elves = line.split(",");
+  const firstElfTasks = elves[0].split("-").map(Number);
+  const secondElfTasks = elves[1].split("-").map(Number);
+
+  // parsing ranges into an array of numbers
+  let firstElfTasksParsed = [];
+  let secondElfTasksParsed = [];
+  for (let index = firstElfTasks[0]; index <= firstElfTasks[1]; index++) {
+    firstElfTasksParsed.push(index);
+  }
+  for (let index = secondElfTasks[0]; index <= secondElfTasks[1]; index++) {
+    secondElfTasksParsed.push(index);
+  }
+
+  // finding overlaps
+  const overlap = _.intersection(firstElfTasksParsed, secondElfTasksParsed);
+
+  // we only care if there is any overlap at all, ignore zero overlaps
+  if (overlap.length > 0) {
+    // if there is, check if it's a complete overlap
+    if (
+      overlap.length === firstElfTasksParsed.length ||
+      overlap.length === secondElfTasksParsed.length
+    ) {
+      completeOverlaps++;
+    }
+  }
+});
+
+console.log(completeOverlaps);
