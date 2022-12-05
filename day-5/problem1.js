999c26172020325411aa4764c4c9dfe88aada2ee
Alan Hong <xiupan@gmail.com>
2022-12-05 12:27:17 -0600

    feat: day 5 problem 1 complete!

---
+++ day-5/problem1.js
@@ -0,0 +1,54 @@
+import { boxes, craneInstructions } from "./input.js";
+
+/* 
+[H]                 [Z]         [J]
+[L]     [W] [B]     [G]         [R]
+[R]     [G] [S]     [J] [H]     [Q]
+[F]     [N] [T] [J] [P] [R]     [F]
+[B]     [C] [M] [R] [Q] [F] [G] [P]
+[C] [D] [F] [D] [D] [D] [T] [M] [G]
+[J] [C] [J] [J] [C] [L] [Z] [V] [B]
+[M] [Z] [H] [P] [N] [W] [P] [L] [C]
+ 1   2   3   4   5   6   7   8   9 
+*/
+
+// bottom to top
+const boxMap = {
+  1: ["M", "J", "C", "B", "F", "R", "L", "H"],
+  2: ["Z", "C", "D"],
+  3: ["H", "J", "F", "C", "N", "G", "W"],
+  4: ["P", "J", "D", "M", "T", "S", "B"],
+  5: ["N", "C", "D", "R", "J"],
+  6: ["W", "L", "D", "Q", "P", "J", "G", "Z"],
+  7: ["P", "Z", "T", "F", "R", "H"],
+  8: ["L", "V", "M", "G"],
+  9: ["C", "B", "G", "P", "F", "Q", "R", "J"],
+};
+
+const instructionsArray = craneInstructions.split("\n");
+const parseInstruction = (instruction) => {
+  // move `number` boxes from `from` to `to`
+  const instArr = instruction.split(" ");
+  const numberToMove = parseInt(instArr[1]);
+  const from = parseInt(instArr[3]);
+  const to = parseInt(instArr[5]);
+  return { numberToMove, from, to };
+};
+
+instructionsArray.forEach((instruction) => {
+  const { numberToMove, from, to } = parseInstruction(instruction);
+  // move boxes from `from` to `to`
+  for (let index = 1; index <= numberToMove; index++) {
+    const box = boxMap[from].pop();
+    if (!box) {
+      // this would indicate a problem with the loop/logic
+      throw new Error("No box to move");
+    }
+    boxMap[to].push(box);
+  }
+});
+
+const lastBoxInEachRow = Object.values(boxMap).map(
+  (row) => row[row.length - 1]
+);
+console.log(lastBoxInEachRow.join(""));
