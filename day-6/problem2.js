69c1226bbde896a2e8a9272bf1964b4e03dc62e0
Alan Hong <xiupan@gmail.com>
2022-12-06 10:57:22 -0600

    feat: day 6 problem 2 complete!

--- day-6/problem2.js
+++ day-6/problem2.js
@@ -3,11 +3,11 @@ import { input } from "./input.js";
 let targetIndex = 0;
 
 for (let index = 0; index < input.length; index++) {
-  if (index >= 3) {
-    const start = index - 3;
+  if (index >= 13) {
+    const start = index - 13;
     const end = index + 1;
     const uniqueArr = [...new Set(input.slice(start, end))];
-    if (uniqueArr.length === 4) {
+    if (uniqueArr.length === 14) {
       targetIndex = end;
       break;
     }
